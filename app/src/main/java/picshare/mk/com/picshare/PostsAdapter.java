49dcd2f3dfa456c049562d5ce06bb1544b5f2d4d
Salim <salim7ariz@gmail.com>
2016-04-26 19:37:43 -0400

    Fix profile pic

--- app/src/main/java/picshare/mk/com/picshare/PostsAdapter.java
+++ app/src/main/java/picshare/mk/com/picshare/PostsAdapter.java
@@ -4,6 +4,7 @@ import android.app.ActivityManager;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Build;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -20,6 +21,8 @@ import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
 import com.nostra13.universalimageloader.core.assist.SimpleImageLoadingListener;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.HashMap;
 import java.util.List;
 
@@ -35,18 +38,21 @@ public class PostsAdapter extends ArrayAdapter<Post> {
     AppUtils appU;
     private ImageLoader imageLoader;
     private DisplayImageOptions options;
+    ImageView prof;
+
     public PostsAdapter(Context context, List<Post> Posts) {
         super(context, 0, Posts);
     }
+
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         final int[] nbrLikes = new int[1];
-        if(convertView == null){
-            convertView = LayoutInflater.from(getContext()).inflate(R.layout.posts_list_layout,parent, false);
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.posts_list_layout, parent, false);
         }
-        appU= new AppUtils();
+        appU = new AppUtils();
         TweetViewHolder viewHolder = (TweetViewHolder) convertView.getTag();
-        if(viewHolder == null){
+        if (viewHolder == null) {
             viewHolder = new TweetViewHolder();
             viewHolder.userName = (TextView) convertView.findViewById(R.id.userName);
             viewHolder.likes = (TextView) convertView.findViewById(R.id.nbr_likes);
@@ -60,19 +66,19 @@ public class PostsAdapter extends ArrayAdapter<Post> {
 
         Post post = getItem(position);
         viewHolder.userName.setText(post.getUserName());
-        viewHolder.likes.setText(post.getLikes()+" Likes");
-        nbrLikes[0] =Integer.parseInt(post.getLikes());
+        viewHolder.likes.setText(post.getLikes() + " Likes");
+        nbrLikes[0] = Integer.parseInt(post.getLikes());
         if (gps.canGetLocation()) {
-            HashMap<String,Double> coord = appU.getCoordonatesFromString(post.getLocation());
+            HashMap<String, Double> coord = appU.getCoordonatesFromString(post.getLocation());
             viewHolder.postLocation.setText(gps.getLocationName(coord.get("latitude"), coord.get("longitude")));
-        }else{
+        } else {
             viewHolder.postLocation.setText("No Available location");
         }
         initImageLoader();
         imageLoader = ImageLoader.getInstance();
         options = new DisplayImageOptions.Builder()
-                .showStubImage(R.drawable.loading)		//	Display Stub Image
-                .showImageForEmptyUri(R.drawable.loading)	//	If Empty image found
+                .showStubImage(R.drawable.loading)        //	Display Stub Image
+                .showImageForEmptyUri(R.drawable.loading)    //	If Empty image found
                 .cacheInMemory()
                 .cacheOnDisc().bitmapConfig(Bitmap.Config.RGB_565).build();
         //Setting Images
@@ -85,31 +91,32 @@ public class PostsAdapter extends ArrayAdapter<Post> {
                                                       View view, Bitmap loadedImage) {
                             super.onLoadingComplete(imageUri, view,
                                     loadedImage);
- }
+                        }
                     });
         } else {
             viewHolder.picture.setImageResource(R.drawable.loading);
         }
         //
         DownloadImg down = new DownloadImg();
-        down.getImage((ImageView) convertView.findViewById(R.id.userPic), post.getUserAvatar());
+        prof = (ImageView) convertView.findViewById(R.id.userPic);
+        new LoadImage().execute(post.getUserAvatar());
+
         final TweetViewHolder finalViewHolder = viewHolder;
         final TweetViewHolder finalViewHolder1 = viewHolder;
         viewHolder.likesIcon.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              //  assert(R.id..heart_full == finalViewHolder.likesIcon.getDrawable());
-                if(finalViewHolder.likesIcon.getDrawable().getConstantState()== getContext().getResources().getDrawable(R.drawable.heart_empty).getConstantState() )
-                {
+                //  assert(R.id..heart_full == finalViewHolder.likesIcon.getDrawable());
+                if (finalViewHolder.likesIcon.getDrawable().getConstantState() == getContext().getResources().getDrawable(R.drawable.heart_empty).getConstantState()) {
                     finalViewHolder.likesIcon.setImageResource(R.drawable.heart_full);
                     nbrLikes[0]++;
-                    finalViewHolder1.likes.setText(nbrLikes[0]+" Likes");
+                    finalViewHolder1.likes.setText(nbrLikes[0] + " Likes");
                     //TODO Save the number of likes and the post id in localStorage to update the data base when the user closes the app.
 
-                }else{
+                } else {
                     finalViewHolder.likesIcon.setImageResource(R.drawable.heart_empty);
                     nbrLikes[0]--;
-                    finalViewHolder1.likes.setText(nbrLikes[0]+" Likes");
+                    finalViewHolder1.likes.setText(nbrLikes[0] + " Likes");
                     //TODO Save the number of likes and the post id in localStorage to update the data base when the user closes the app.
 
                 }
@@ -119,7 +126,8 @@ public class PostsAdapter extends ArrayAdapter<Post> {
 
         return convertView;
     }
-    private class TweetViewHolder{
+
+    private class TweetViewHolder {
         public TextView userName;
         public TextView likes;
         public TextView postLocation;
@@ -128,6 +136,7 @@ public class PostsAdapter extends ArrayAdapter<Post> {
         public ImageView likesIcon;
 
     }
+
     private void initImageLoader() {
         int memoryCacheSize;
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ECLAIR) {
@@ -143,12 +152,39 @@ public class PostsAdapter extends ArrayAdapter<Post> {
                 getContext()).threadPoolSize(5)
                 .threadPriority(Thread.NORM_PRIORITY - 2)
                 .memoryCacheSize(memoryCacheSize)
-                .memoryCache(new FIFOLimitedMemoryCache(memoryCacheSize-1000000))
+                .memoryCache(new FIFOLimitedMemoryCache(memoryCacheSize - 1000000))
                 .denyCacheImageMultipleSizesInMemory()
                 .discCacheFileNameGenerator(new Md5FileNameGenerator())
                 .tasksProcessingOrder(QueueProcessingType.LIFO).enableLogging()
                 .build();
 
         ImageLoader.getInstance().init(config);
+    }
+
+    private class LoadImage extends AsyncTask<String, String, Bitmap> {
+        Bitmap bitmap;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        protected Bitmap doInBackground(String... args) {
+            try {
+                bitmap = BitmapFactory.decodeStream((InputStream) new URL(args[0]).getContent());
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap image) {
+
+            if (image != null) {
+                prof.setImageBitmap(image);
+            }
+        }
     }
 }
